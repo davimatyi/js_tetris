b82a0951899a5aa39539407ba20986d640f28608
Dávid Mátyás <jxtbv0@inf.elte.hu>
2022-01-07 15:10:58 +0100

    Menu ui using the new library

--- sketch/sketch.js
+++ sketch/sketch.js
@@ -8,8 +8,9 @@ let swapped = false;
 
 let droptimer = 0, moveTimer = 0;
 
-let gameLost = false;
+let gameOver = false;
 let gamePaused = false;
+let mainMenu = true;
 
 let clearedLines = 0;
 let level = 0;
@@ -44,12 +45,230 @@ function initGame() {
     ind = Math.floor(Math.random() * tetrominoes.length);
     nextMino = new Tetromino(ind, initialPosition, field);
 
-    gameLost = false;
+    gameOver = false;
     gamePaused = false;
+    mainMenu = false;
     swapped = false;
     score = 0;
     level = 0;
     clearedLines = 0;
+}
+
+function checkTetris() {
+    let lines = 0;
+    for (let i = mapHeight - 1; i >= 0; i--) {
+        let row = true;
+        for (let j = 0; j < mapWidth; j++) {
+            if (!(field[j][i] && row)) row = false;
+        }
+        if (row) {
+            for (let k = i; k >= 0; k--) {
+                for (let j = 0; j < mapWidth; j++) {
+                    field[j][k] = field[j][k - 1];
+                }
+            }
+            for (let j = 0; j < mapWidth; j++) {
+                field[j][0] = 0;
+            }
+            i++;
+            lines++;
+        }
+    }
+    clearedLines += lines;
+    level = Math.floor(clearedLines / 10);
+    switch (lines) {
+        case 0: break;
+        case 1:
+            score += 40 * (level + 1);
+            break;
+        case 2:
+            score += 100 * (level + 1);
+            break;
+        case 3:
+            score += 300 * (level + 1);
+            break;
+        default:
+            score += 1200 * (level + 1);
+    }
+}
+
+function checkFail() {
+    gameOver = gameOver || currentMino.checkFail();
+    if(gameOver) filter(BLUR, 5);
+}
+
+function drop() {
+    if (gameOver || gamePaused) return;
+    droptimer = 0;
+    let b = currentMino.drop();
+    if (b) {
+        currentMino = nextMino;
+        let ind = Math.floor(Math.random() * tetrominoes.length);
+        nextMino = new Tetromino(ind, initialPosition, field);
+        checkTetris();
+        checkFail();
+        swapped = false;
+    } else {
+        if (keyIsDown(DOWN_ARROW))
+            score += 1;
+    }
+}
+function hardDrop() {
+    if (gameOver || gamePaused) return;
+    dropTimer = 0;
+    let p = currentMino.hardDrop();
+    currentMino = nextMino;
+    let ind = Math.floor(Math.random() * tetrominoes.length);
+    nextMino = new Tetromino(ind, initialPosition, field);
+    checkTetris();
+    checkFail();
+    swapped = false;
+    score += 2 * p;
+}
+
+function spin() {
+    if (gameOver || gamePaused) return;
+    currentMino.spin();
+}
+
+function moveLeft() {
+    if (gameOver || gamePaused) return;
+    currentMino.moveLeft();
+}
+
+function moveRight() {
+    if (gameOver || gamePaused) return;
+    currentMino.moveRight();
+}
+
+function hold() {
+    if (gameOver || gamePaused || swapped) return;
+    if (heldMino == null) {
+        heldMino = currentMino;
+        currentMino = nextMino;
+        let ind = Math.floor(Math.random() * tetrominoes.length);
+        nextMino = new Tetromino(ind, initialPosition, field);
+        checkFail();
+    } else {
+        let temp = heldMino;
+        heldMino = currentMino;
+        currentMino = temp;
+        currentMino.setLocation(initialPosition);
+    }
+    swapped = true;
+}
+
+function keyPressed() {
+    switch (keyCode) {
+        case LEFT_ARROW:
+            moveLeft();
+            break;
+        case RIGHT_ARROW:
+            moveRight();
+            break;
+        case UP_ARROW:
+            spin();
+            break;
+        case DOWN_ARROW:
+            drop();
+            break;
+        case 32:
+            if (gameOver) initGame();
+            else hardDrop();
+            break;
+        case 27:
+            gamePaused = !gamePaused;
+            if(gamePaused) filter(BLUR, 5);
+            break;
+        case 67:
+            hold();
+            break;
+    }
+}
+
+function renderGameUi() {
+    clear();
+    fill(255);
+    stroke(120);
+
+    rect(mapX, mapY, mapWidth * squaresize, mapHeight * squaresize);
+    scoreLabel.setText(score);
+    levelLabel.setText(level);
+    linesLabel.setText(clearedLines);
+
+    nextPieceWindow.draw();
+    heldPieceWindow.draw();
+    scoreWindow.draw();
+    levelWindow.draw();
+    linesWindow.draw();
+}
+
+function renderTetrominoes() {
+    nextMino.drawAtPos([mapX + mapWidth * squaresize + 4.5 * squaresize,
+    mapY + 2.5 * squaresize]);
+
+    if (heldMino != null) {
+        heldMino.drawAtPos([mapX + mapWidth * squaresize + 4.5 * squaresize,
+        mapY + 9.5 * squaresize]);
+    }
+
+    for (let i = 0; i < mapWidth; i++) {
+        for (let j = 0; j < mapHeight; j++) {
+            if (field[i][j]) {
+                fill(colors[field[i][j] - 1])
+                rect(mapX + i * squaresize, mapY + j * squaresize, squaresize, squaresize, 5);
+            }
+        }
+    }
+    currentMino.drawGhost();
+    currentMino.drawOnMap();
+}
+
+function updateGame() {
+    if (!gameOver && !gamePaused) {
+        if (droptimer > Math.max(minDropInterval, dropInterval - level / 30)) drop();
+        droptimer += deltaTime / 1000.0;
+    }
+
+    if (keyIsDown(DOWN_ARROW)) {
+        moveTimer += deltaTime / 1000.0;
+        if (moveTimer > initialMoveDelay) {
+            if (moveTimer > initialMoveDelay + moveInterval) {
+                drop();
+                moveTimer = initialMoveDelay;
+            }
+        }
+
+    } else if (keyIsDown(LEFT_ARROW)) {
+        moveTimer += deltaTime / 1000.0;
+        if (moveTimer > initialMoveDelay) {
+            if (moveTimer > initialMoveDelay + moveInterval) {
+                moveLeft();
+                moveTimer = initialMoveDelay;
+            }
+        }
+
+    } else if (keyIsDown(RIGHT_ARROW)) {
+        moveTimer += deltaTime / 1000.0;
+        if (moveTimer > initialMoveDelay) {
+            if (moveTimer > initialMoveDelay + moveInterval) {
+                moveRight();
+                moveTimer = initialMoveDelay;
+            }
+        }
+    } else {
+        moveTimer = 0;
+    }
+}
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    frameRate(200);
+
+    initGame();
+    mainMenu = true;
+    highscores = getItem('highscores');
+    // TODO finish highscores
 
     nextPieceWindow = new Window(mapX + mapWidth * squaresize + 2 * squaresize, mapY, 6 * squaresize, 6 * squaresize);
     heldPieceWindow = new Window(mapX + mapWidth * squaresize + 2 * squaresize, mapY + 7 * squaresize, 6 * squaresize, 6 * squaresize);
@@ -71,235 +290,60 @@ function initGame() {
     scoreWindow.addComponent(scoreLabel);
     levelWindow.addComponent(levelLabel);
     linesWindow.addComponent(linesLabel);
-}
 
-function checkTetris() {
-    let lines = 0;
-    for(let i = mapHeight -1; i >= 0; i--) {
-        let row = true;
-        for(let j = 0; j < mapWidth; j++) {
-            if(!(field[j][i] && row)) row = false;
-        }
-        if(row) {
-            for(let k = i; k >= 0; k--) {
-                for(let j = 0; j < mapWidth; j++) {
-                    field[j][k] = field[j][k-1];
-                }
-            }
-            for(let j = 0; j < mapWidth; j++) {
-                field[j][0] = 0;
-            }
-            i++;
-            lines++;
-        }
-    }
-    clearedLines += lines;
-    level = Math.floor(clearedLines / 10);
-    switch(lines) {
-        case 0: break;
-        case 1: 
-            score += 40 * (level + 1);
-            break;
-        case 2:
-            score += 100 * (level + 1);
-            break;
-        case 3:
-            score += 300 * (level + 1);
-            break;
-        default:
-            score += 1200 * (level + 1);
-    }
-}
+    menuWindow = new Window(canvasWidth / 2 - 200, 75, 400, 550);
+    pauseWindow = new Window(canvasWidth / 2 - 200, 100, 400, 450);
+    gameOverWindow = new Window(canvasWidth / 2 - 200, 100, 400, 500);
 
-function checkFail() {
-    gameLost = gameLost || currentMino.checkFail();
-}
+    let menuNewGameButton = new Button(canvasWidth / 2 - 100, 175, 200, 75, "New game");
+    let pauseNewGameButton = new Button(canvasWidth / 2 - 100, 350, 200, 60, "New game");
+    let overNewGameButton = new Button(canvasWidth / 2 - 100, 200, 200, 60, "New game");
+    let resumeButton = new Button(canvasWidth / 2 - 100, 250, 200, 60, "Resume");
+    let pauseExitButton = new Button(canvasWidth / 2 - 100, 450, 200, 60, "Exit to menu");
+    let overExitButton = new Button(canvasWidth / 2 - 100, 300, 200, 60, "Exit to menu");
+    let menuExitButton = new Button(canvasWidth / 2 - 100, 300, 200, 60, "Quit");
+    menuNewGameButton.onClick(initGame);
+    pauseNewGameButton.onClick(initGame);
+    overNewGameButton.onClick(initGame);
+    resumeButton.onClick(function () {
+        gamePaused = false;
+    });
+    pauseExitButton.onClick(function() {
+        gamePaused = false;
+        mainMenu = true;
+        filter(BLUR, 5);
+    });
+    overExitButton.onClick(function () {
+        gameOver = false;
+        mainMenu = true;
+        filter(BLUR, 5);
+    });
+    menuWindow.addComponent(menuNewGameButton);
+    menuWindow.addComponent(menuExitButton);
+    pauseWindow.addComponent(pauseNewGameButton);
+    pauseWindow.addComponent(resumeButton);
+    pauseWindow.addComponent(pauseExitButton);
+    gameOverWindow.addComponent(overNewGameButton);
+    gameOverWindow.addComponent(overExitButton);
+    menuWindow.addComponent(new Label("Tetris", canvasWidth / 2, 125));
+    pauseWindow.addComponent(new Label("Paused", canvasWidth / 2, 150));
+    gameOverWindow.addComponent(new Label("Game over", canvasWidth / 2, 150));
 
-function drop() {
-    if(gameLost || gamePaused) return;
-    droptimer = 0;
-    let b = currentMino.drop();
-    if (b) {
-        currentMino = nextMino;
-        let ind = Math.floor(Math.random() * tetrominoes.length);
-        nextMino = new Tetromino(ind, initialPosition, field);
-        checkTetris();
-        checkFail();
-        swapped = false;
-    } else {
-        if(keyIsDown(DOWN_ARROW))
-            score += 1;
-    }
-}
-function hardDrop() {
-    if(gameLost || gamePaused) return;
-    dropTimer = 0;
-    let p = currentMino.hardDrop();
-    currentMino = nextMino;
-    let ind = Math.floor(Math.random() * tetrominoes.length);
-    nextMino = new Tetromino(ind, initialPosition, field);
-    checkTetris();
-    checkFail();
-    swapped = false;
-    score += 2 * p;
-}
-
-function spin() {
-    if(gameLost || gamePaused) return;
-    currentMino.spin();
-}
-
-function moveLeft() {
-    if(gameLost || gamePaused) return;
-    currentMino.moveLeft();
-}
-
-function moveRight() {
-    if(gameLost || gamePaused) return;
-    currentMino.moveRight();
-}
-
-function hold() {
-    if(gameLost || gamePaused || swapped) return;
-    if(heldMino == null) {
-        heldMino = currentMino;
-        currentMino = nextMino;
-        let ind = Math.floor(Math.random() * tetrominoes.length);
-        nextMino = new Tetromino(ind, initialPosition, field);
-        checkFail();
-    } else {
-        let temp = heldMino;
-        heldMino = currentMino;
-        currentMino = temp;
-        currentMino.setLocation(initialPosition);
-    }
-    swapped = true;
-}
-
-function keyPressed() {
-    switch(keyCode) {
-        case LEFT_ARROW: 
-            moveLeft();
-            break;
-        case RIGHT_ARROW:
-            moveRight();
-            break;
-        case UP_ARROW:
-            spin();
-            break;
-        case DOWN_ARROW:
-            drop();
-            break;
-        case 32:
-            if(gameLost) initGame();
-            else hardDrop();
-            break;
-        case 27:
-            gamePaused = ! gamePaused;
-            break;
-        case 67:
-            hold();
-            break;
-    }   
-}
-
-function setup() {
-    createCanvas(canvasWidth, canvasHeight);
-    frameRate(200);
-
-    initGame();    
-    highscores = getItem('highscores');
-    // TODO finish highscores
+    renderGameUi();
+    filter(BLUR, 5);
 }
 
 function draw() {
-    clear();
-    fill(255);
-    stroke(120);
-
-    rect(mapX, mapY, mapWidth * squaresize, mapHeight * squaresize);
-    scoreLabel.setText(score);
-    levelLabel.setText(level);
-    linesLabel.setText(clearedLines);
-
-    nextPieceWindow.draw();
-    heldPieceWindow.draw();
-    scoreWindow.draw();
-    levelWindow.draw();
-    linesWindow.draw();
-
-    nextMino.drawAtPos([mapX + mapWidth * squaresize + 4.5 * squaresize,
-                        mapY + 2.5 * squaresize]);
-
-    if(heldMino != null) {
-        heldMino.drawAtPos([mapX + mapWidth * squaresize + 4.5 * squaresize,
-                            mapY + 9.5 * squaresize]);
-    }
-
-    for (let i = 0; i < mapWidth; i++) {
-        for (let j = 0; j < mapHeight; j++) {
-            if (field[i][j]) {
-                fill(colors[field[i][j] - 1])
-                rect(mapX + i * squaresize, mapY + j * squaresize, squaresize, squaresize, 5);
-            }
-        }
-    }
-    currentMino.drawGhost();
-    currentMino.drawOnMap();
-
-    if(! gameLost && ! gamePaused) {
-        if (droptimer > Math.max(minDropInterval, dropInterval - level / 30)) drop();
-        droptimer += deltaTime / 1000.0;
-    }
-
-    if(keyIsDown(DOWN_ARROW)) {
-        moveTimer += deltaTime / 1000.0;
-        if(moveTimer > initialMoveDelay) {
-            if(moveTimer > initialMoveDelay + moveInterval) {
-                drop();
-                moveTimer = initialMoveDelay;
-            }
-        }
-
-    } else if(keyIsDown(LEFT_ARROW)) {
-        moveTimer += deltaTime / 1000.0;
-        if(moveTimer > initialMoveDelay) {
-            if(moveTimer > initialMoveDelay + moveInterval) {
-                moveLeft();
-                moveTimer = initialMoveDelay;
-            }
-        }
-
-    } else if(keyIsDown(RIGHT_ARROW)) {
-        moveTimer += deltaTime / 1000.0;
-        if(moveTimer > initialMoveDelay) {
-            if(moveTimer > initialMoveDelay + moveInterval) {
-                moveRight();
-                moveTimer = initialMoveDelay;
-            }
-        }
+    if (mainMenu) {
+        menuWindow.draw();
+    } else if (gamePaused) {
+        pauseWindow.draw();
+    } else if (gameOver) {
+        gameOverWindow.draw();
     } else {
-        moveTimer = 0;
+        renderGameUi();
+        renderTetrominoes();
+        updateGame();
     }
 
-
-    if(gamePaused) {
-        // filter(BLUR, 10);
-        fill(255);
-        rect(mapX + (mapWidth * squaresize) / 2 - 100, mapY + (mapHeight * squaresize) / 2 - 25, 200, 50);
-        fill(0);
-        textSize(32);
-        textAlign(CENTER, CENTER);
-        text("Paused", mapX + (mapWidth * squaresize) / 2, mapY + (mapHeight * squaresize) / 2);
-    }
-    if(gameLost) {
-        fill(255);
-        rect(mapX + (mapWidth * squaresize) / 2 - 100, mapY + (mapHeight * squaresize) / 2 - 40, 200, 80);
-        fill(0);
-        textSize(32);
-        textAlign(CENTER, CENTER);
-        text("Game over", mapX + (mapWidth * squaresize) / 2, mapY + (mapHeight * squaresize) / 2 - 10);
-        textSize(14);
-        text("Press space to restart", mapX + (mapWidth * squaresize) / 2, mapY + (mapHeight * squaresize) / 2 + 18)
-    }
 }
